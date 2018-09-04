15f060cafd18e7c3ad34ed3ea8ce04fae44d1e6b
Rachid B Racö <mail.racorp@gmail.com>
2018-09-04 15:20:24 +0200

    reupload personnal repository on github

---
+++ README.md
@@ -0,0 +1,21 @@
+<center>
+# Ruby Gem GoogleDrive Spreadsheet
+
+-------------
+
+**Reupload my personnal repository on github.**
+maybe it can be helpful ;)
+
+-------------
+
+**Reupload mon dossier sur github.**
+Peut-être que ça peut vous être utile ;)
+
+-------------
+Exemple d'utilisation de la gem GoogleDrive Spreadsheet Ruby
+-------------
+
+
+</center>
+
+Rachid
---
+++ google-drive-spreadsheets.rb
@@ -0,0 +1,39 @@
+require "google_drive"
+
+# Creates a session. This will prompt the credential via command line for the
+# first time and save it to config.json file for later usages.
+# See this document to learn how to create config.json:
+# https://github.com/gimite/google-drive-ruby/blob/master/doc/authorization.md
+session = GoogleDrive::Session.from_config("config.json")
+
+# First worksheet of
+# https://docs.google.com/spreadsheet/ccc?key=pz7XtlQC-PYx-jrVMJErTcg
+# Or https://docs.google.com/a/someone.com/spreadsheets/d/pz7XtlQC-PYx-jrVMJErTcg/edit?usp=drive_web
+ws = session.spreadsheet_by_key("1smypkyAz4STrKO4Zkos5Z4UPUJKvvgIza32LnlQ7OGw").worksheets[0]
+
+# Gets content of A2 cell.
+p ws[2, 1]  #==> "hoge"
+
+# Changes content of cells.
+# Changes are not sent to the server until you call ws.save().
+ws[2, 1] = "racö was here ^^)"
+ws[2, 2] = "Hell Yeah !"
+ws.save
+
+# Dumps all cells.
+(1..ws.num_rows).each do |row|
+  (1..ws.num_cols).each do |col|
+    p ws[row, col]
+  end
+end
+
+# Yet another way to do so.
+p ws.rows  #==> [["fuga", ""], ["foo", "bar]]
+
+# Reloads the worksheet to get changes by other clients.
+ws.reload
+
+
+#et sinon pour créer une spreadsheet la commande c'est : ```spreadsheet = google_drive_session.create_spreadsheet(title = "nom_de_la_sheet")```
+
+#Andreas Banelli : par titre ça marche aussi '''spreadsheet_by_title("Mails_listing_mairie").worksheets[0]'''
